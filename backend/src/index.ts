aa01e677dab94b299af4b621ca2199d3f990b41d
Mujtaba <mujtabarasheed20@gmail.com>
2026-02-27 01:57:26 +0500

    feat: implement user authentication and synchronization hooks, add API functions for product and comment management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express()
 
 
 
-app.use(cors({origin:ENV.FRONTEND_URL}));
+app.use(cors({origin:ENV.FRONTEND_URL,credentials:true}));
 app.use(clerkMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
