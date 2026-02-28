a0e722dac0a220707d91276335d16b6bca12095c
Mujtaba <mujtabarasheed20@gmail.com>
2026-03-01 00:26:53 +0500

    update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,20 @@
 import { useProducts } from "../hook/useProducts"
+import LoadingSpinner from "../components/LoadingSpinner"  // add this
 
 const HomePage = () => {
- const {data:products,isLoading , error } =  useProducts
- if(isLoading) return <LoadingSpinner/>;
+  const {data:products, isLoading, error} = useProducts()  // also add () here!
+  
+  if(isLoading) return <LoadingSpinner/>;
 
- if(error) {
-     return (    
-    <div role="alert" className="alert alert-error">
-      <span>Something went wrong. Please refresh the page. </span>
-    </div>
-  )
- };
- return <div>HomePage</div>
- 
+  if(error) {
+    return (    
+      <div role="alert" className="alert alert-error">
+        <span>Something went wrong. Please refresh the page.</span>
+      </div>
+    )
+  };
+  
+  return <div>HomePage</div>
 }
 
 export default HomePage
